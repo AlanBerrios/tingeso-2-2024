7042ec7cd7ec5a22bdab956b8b21196c9e43996d
AlanBerrios <112836766+AlanBerrios@users.noreply.github.com>
2024-11-04 01:17:40 -0300

    pom

--- backendprestabanco/src/test/BackendPrestaBancoApplicationTest.java
+++ backendprestabanco/src/test/BackendPrestaBancoApplicationTest.java
@@ -1,10 +1,10 @@
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-// @SpringBootTest
-// public class BackendPrestaBancoApplicationTest {
+@SpringBootTest
+public class BackendPrestaBancoApplicationTest {
 
-    // @Test
-   // void contextLoads() {
-   // }
-// }
+    @Test
+   void contextLoads() {
+   }
+}

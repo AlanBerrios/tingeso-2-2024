1456b1cee17fc714cbd0440994fda3c904ea38ed
AlanBerrios <112836766+AlanBerrios@users.noreply.github.com>
2024-11-06 17:00:46 -0300

    a

--- backendprestabanco/src/main/java/com/example/backendprestabanco/repositories/ClientRepository.java
+++ backendprestabanco/src/main/java/com/example/backendprestabanco/repositories/ClientRepository.java
@@ -2,6 +2,7 @@ package com.example.backendprestabanco.repositories;
 
 import com.example.backendprestabanco.entities.ClientEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -13,6 +14,11 @@ public interface ClientRepository extends JpaRepository<ClientEntity, String> {
     public ClientEntity findByRut(String rut);
     public void deleteByRut(String rut);
     public boolean existsByRut(String rut);
+
+    // Query nativa para eliminar un cliente por su RUT
+    @Modifying
+    @Query(value = "DELETE FROM clients WHERE clients.rut = :rut", nativeQuery = true)
+    void deleteByRutNativeQuery(@Param("rut") String rut);
 
     // Query nativo para encontrar un cliente por su RUT
     @Query(value = "SELECT * FROM clients WHERE clients.rut = :rut", nativeQuery = true)
--- backendprestabanco/src/main/java/com/example/backendprestabanco/services/ClientService.java
+++ backendprestabanco/src/main/java/com/example/backendprestabanco/services/ClientService.java
@@ -29,7 +29,7 @@ public class ClientService {
     }
 
     public void deleteClient(String rut) {
-        clientRepository.deleteByRut(rut);
+        clientRepository.deleteByRutNativeQuery(rut);
         return;
     }
 

d0d978a3fd115a8a7f9e26ef24ac9b7640297b74
AlanBerrios <112836766+AlanBerrios@users.noreply.github.com>
2024-11-05 23:29:47 -0300

    Update ClientList.jsx

--- frontendprestabanco/src/components/ClientList.jsx
+++ frontendprestabanco/src/components/ClientList.jsx
@@ -21,12 +21,15 @@ export default function ClientList() {
 
   const handleDelete = async (rut) => {
     try {
-      await gestionService.deleteClientByRut(rut);
+      const response = await gestionService.deleteClientByRut(rut);
+      console.log(response); // Para verificar la respuesta
       setClients(clients.filter((client) => client.rut !== rut));
     } catch (error) {
+      console.error(error); // Ver error en consola
       alert("Error al eliminar el cliente.");
     }
   };
+  
 
   return (
     <div className="container">

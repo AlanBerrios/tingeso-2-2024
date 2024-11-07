4e41b7a20229201c263ef8393289d4a156f85093
AlanBerrios <112836766+AlanBerrios@users.noreply.github.com>
2024-11-07 08:38:47 -0300

    Update Home.jsx

--- frontendprestabanco/src/components/Home.jsx
+++ frontendprestabanco/src/components/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     <div className="home-container">
       <div className="background-image"></div>
       <div className="content">
-        <h1>Bienvenido a PrestaBanco, donde tu dinero queda en buenas manos. :)</h1>
+        <h1>Bienvenido a PrestaBanco, donde tu dinero queda en buenas manos. :) :)</h1>
         <p></p>
         <p>Esta aplicacion permite trabajar con una base de datos con la creacion de Cuentas y Prestamos.</p>
         <p>Para comenzar, navega por la barra de navegacion y selecciona la opcion que desees.</p>
